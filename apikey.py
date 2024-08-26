c0527860eb0b31c9635c3a6e996ad59e9dc82c31
m0nerl <monerl2525@gmail.com>
2024-08-26 22:20:37 +0200

    used api key

---
+++ apikey.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+params = {
+    "api_key": "vqWBAPZe0wVToG97IcdcfEIHamZ1zwBI",  
+    "country": "pl",
+    "year": 2024,
+    "month": 12
+}
+
+r = requests.get("https://calendarific.com/api/v2/holidays", params=params)
+
+try:
+    content = r.json()
+except json.decoder.JSONDecodeError: 
+    print("Bad format")
+else:
+    for holiday in content['response']['holidays']:
+        print(f"Name of Holiday: {holiday['name']}")
+        print(f"Description of Holiday: {holiday.get('description', 'No description available')}")
+        print(f"Date of Holiday: {holiday['date']['iso']}")
+        print(f"Type of Holiday: {', '.join(holiday['type'])}")
+        print()  
