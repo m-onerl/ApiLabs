6d24506a1c129967a29dc2f92edbad9fa635e8c3
m0nerl <monerl2525@gmail.com>
2024-09-06 22:53:03 +0200

    doing smth with api

--- Header/logowanieHeader.py
+++ Header/logowanieHeader.py
@@ -1,18 +1,39 @@
 import requests
 import json   
 from pprint import pprint
-from credentials import headers
+import credentials 
 
-r = requests.get('https://api.thecatapi.com/v1/favourites/', headers = headers)
+def get_json_content_from_response(response):
+    try:
+        content = response.json()
+    except json.decoder.JSONDecodeError:
+        print("Bad format") 
+    else:
+        return content
+def get_fav_cats(userId):
+    params = {
+        "sub_id" : userId
+    }
+    r = requests.get('https://api.thecatapi.com/v1/favourites/', params, headers = credentials.headers)
+
+    return get_json_content_from_response(r)
+
+def get_random_cat():
+    r = requests.get('https://api.thecatapi.com/v1/images/search', headers = credentials.headers)
+
+    return get_json_content_from_response(r)
 
 
 
-try:
-    content = r.json()
-except json.decoder.JSONDecodeError:
-    print("Bad format") 
-else:
-    print(content)
+print("Hej, zaloguj sie - podaj login oraz hasło")
 
+userId = "agh2m"
+name = "Sebastian"
 
+print("Witaj " + name + "!")
+favCats = get_fav_cats(userId)
+print("Twoje ulubione kotki to", favCats)
+randomCat = get_random_cat()
+print("Wylosowano kotka", randomCat)
+ 
     
